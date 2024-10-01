b6b3d10234bb3da84e753d71063d6ebbec9fd9c2
Marcus Guerrero <marcus.guerrero61@gmail.com>
2024-10-01 15:16:36 +0800

    Inputting the main board of the game in a define function loop

--- chess_game_source_code.py
+++ chess_game_source_code.py
@@ -76,3 +76,25 @@ piece_list = ['pawn', 'queen', 'king', 'knight', 'rook', 'bishop']
 counter = 0
 winner = ''
 game_over = False
+
+
+# draw main game board
+def draw_board():
+    for center in range(32):
+        column = center % 4
+        row = center // 4
+        if row % 2 == 0:
+            pygame.draw.rect(screen, 'light gray', [600 - (column * 200), row * 100, 100, 100])
+        else:
+            pygame.draw.rect(screen, 'light gray', [700 - (column * 200), row * 100, 100, 100])
+        pygame.draw.rect(screen, 'gray', [0, 800, width, 100])
+        pygame.draw.rect(screen, 'gold', [0, 800, width, 100], 5)
+        pygame.draw.rect(screen, 'gold', [800, 0, 200, height], 5)
+        status_text = ['White: Select a Piece to Move!', 'White: Select a Destination!',
+                       'Black: Select a Piece to Move!', 'Black: Select a Destination!']
+        screen.blit(big_font.render(status_text[turn_step], True, 'black'), (20, 820))
+        for center in range(9):
+            pygame.draw.line(screen, 'black', (0, 100 * center), (800, 100 * center), 2)
+            pygame.draw.line(screen, 'black', (100 * center, 0), (100 * center, 800), 2)
+        screen.blit(medium_font.render('FORFEIT', True, 'black'), (810, 830))
+
