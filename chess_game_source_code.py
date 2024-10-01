8b3aa1209e6d6093da9c0c260f738e442bd5b421
Marcus Guerrero <marcus.guerrero61@gmail.com>
2024-10-01 15:26:15 +0800

    Setting up a define function for all valid piece options in thegame board

--- chess_game_source_code.py
+++ chess_game_source_code.py
@@ -122,3 +122,26 @@ def draw_pieces():
             if selection == center:
                 pygame.draw.rect(screen, 'blue', [black_locations[center][0] * 100 + 1, black_locations[center][1] * 100 + 1,
                                                   100, 100], 2)
+
+
+# function to check all pieces valid options on board
+def check_options(pieces, locations, turn):
+    moves_list = []
+    all_moves_list = []
+    for center in range((len(pieces))):
+        location = locations[center]
+        piece = pieces[center]
+        if piece == 'pawn':
+            moves_list = check_pawn(location, turn)
+        elif piece == 'rook':
+            moves_list = check_rook(location, turn)
+        elif piece == 'knight':
+            moves_list = check_knight(location, turn)
+        elif piece == 'bishop':
+            moves_list = check_bishop(location, turn)
+        elif piece == 'queen':
+            moves_list = check_queen(location, turn)
+        elif piece == 'king':
+            moves_list = check_king(location, turn)
+        all_moves_list.append(moves_list)
+    return all_moves_list
